7db5b5ca5052f108327b21018e6efea8a65c4dbd
Jonathan Kim <jkimbo@gmail.com>
2011-08-05 15:02:00 +0100

    Added css3 transtions to social icons

--- js/script.js
+++ js/script.js
@@ -4,16 +4,28 @@
 
 $(document).ready(function(){
 
-    // Add your jQuery here
     $('header').bigtext();
 
+    var greeting = ["Hello there!", "Yo! ", "Hi ", "Morning!", "Evening guvnor", "Afternoon", "What's up?", "How are you?", "Good day to you", "Looking good!", "Nice haircut!", "Have you lost weight?", "It's been too long!", "Hey!" ];
+    var old_rand_num = 0;
+
+    // Random greeting
     $('#reload').click(function() {
-        var greeting = ["Hello there!", "Yo! ", "Hi ", "Morning!", "Evening guvnor", "Afternoon", "What's up?", "How are you?", "Good day to you" ];
-        console.log(Math.floor(Math.random()*(greeting.length - 1))+1);
-        $('#header').text(greeting[Math.floor(Math.random()*(greeting.length - 1))]);
+        var rand_num = randNum();
+        $('#header').text(greeting[rand_num]);
         $('header').bigtext();
         return false;
     });
+
+    function randNum() {
+        var rand_num = Math.floor(Math.random()*(greeting.length - 1)+1);
+        if(rand_num == old_rand_num){
+            rand_num = randNum();
+        }
+        old_rand_num = rand_num;
+        return rand_num;
+    }
+
 });
 
 
