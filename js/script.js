4eecf6096294e6199babb8ad5dbd9d48472f23cf
Jonathan Kim <jkimbo@gmail.com>
2011-08-05 20:05:46 +0100

    Tweaked random greeting generator and added some more greetings!

--- js/script.js
+++ js/script.js
@@ -6,8 +6,37 @@ $(document).ready(function(){
 
     $('header').bigtext();
 
-    var greeting = ["Hello there!", "Yo! ", "Hi ", "Morning!", "Evening guvnor", "Afternoon", "What's up?", "How are you?", "Good day to you", "Looking good!", "Nice haircut!", "Have you lost weight?", "It's been too long!", "Hey!" ];
-    var old_rand_num = 0;
+    var greeting = [
+        "Hello there!",
+        "Yo! ",
+        "Hi ",
+        "Morning!",
+        "Evening guvnor",
+        "Afternoon",
+        "What's up?",
+        "How are you?",
+        "Good day to you",
+        "Looking good!",
+        "Nice haircut!",
+        "Have you lost weight?",
+        "It's been too long!",
+        "Hey!",
+        "Bonjour!",
+        "How do you do?",
+        "How's life?",
+        "How are things?",
+        "Pleased to meet you",
+        "Fancy seeing you here!",
+        "Good day sir/madam",
+        "G'day mate!",
+        "Wassup?",
+        "Hiya!",
+        "Ey up!",
+        "Greetings!",
+        "Salutations"
+    ];
+
+    var old_rand_num_array = [];
 
     // Random greeting
     $('#reload').click(function() {
@@ -18,11 +47,15 @@ $(document).ready(function(){
     });
 
     function randNum() {
-        var rand_num = Math.floor(Math.random()*(greeting.length - 1)+1);
-        if(rand_num == old_rand_num){
+        var rand_num = Math.floor(Math.random()*(greeting.length - 1));
+        if($.inArray(rand_num, old_rand_num_array) !== -1){
             rand_num = randNum();
         }
-        old_rand_num = rand_num;
+        old_rand_num_array.push(rand_num);
+        if(old_rand_num_array.length > (greeting.length*0.8)){
+            old_rand_num_array = old_rand_num_array.slice(1);
+        }
+        console.log(old_rand_num_array);
         return rand_num;
     }
 
