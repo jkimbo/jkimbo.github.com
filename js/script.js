6676752196617a15ecbfff66b36ad69c41730799
Jonathan Kim <jkimbo@gmail.com>
2011-08-06 12:06:54 +0100

    Improved rand_num generator and added 404 redirect

--- js/script.js
+++ js/script.js
@@ -41,6 +41,10 @@ $(document).ready(function(){
     // Random greeting
     $('#reload').click(function() {
         var rand_num = randNum();
+        old_rand_num_array.push(rand_num);
+        if(old_rand_num_array.length > (greeting.length*0.7)){
+            old_rand_num_array = old_rand_num_array.slice(1);
+        }
         $('#header').text(greeting[rand_num]);
         $('header').bigtext();
         return false;
@@ -48,17 +52,12 @@ $(document).ready(function(){
 
     function randNum() {
         var rand_num = Math.floor(Math.random()*(greeting.length - 1));
-        if($.inArray(rand_num, old_rand_num_array) !== -1){
-            rand_num = randNum();
+        if($.inArray(rand_num, old_rand_num_array) !== -1){ // if number is in array
+            return randNum();
+        } else { // number is not in array
+            return rand_num;
         }
-        old_rand_num_array.push(rand_num);
-        if(old_rand_num_array.length > (greeting.length*0.8)){
-            old_rand_num_array = old_rand_num_array.slice(1);
-        }
-        console.log(old_rand_num_array);
-        return rand_num;
     }
-
 });
 
 
