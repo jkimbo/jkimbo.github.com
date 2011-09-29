77ab2778900b66c62148f1388991bb447874a517
Jonathan Kim <jkimbo@gmail.com>
2011-09-29 01:45:03 +0100

    Reverted back to before update with html5 boilerplate to rectify site causing chrome to crash Revert "Added 404 page and tweaked reload algorithm"
    This reverts commit 606672f0ff7944deacc2244a3065890fb695085f.

--- js/script.js
+++ js/script.js
@@ -36,22 +36,11 @@ $(document).ready(function(){
         "Salutations"
     ];
 
-    var error = [
-        "Oh poo!",
-        "Goodness gracious great balls of fire!",
-        "Well this is embarrassing...",
-        "Errmm...",
-        "F***ing hell!",
-        "Oh man!",
-        "Shoot!",
-        "Dagnabbit!"
-    ];
-
     var old_rand_num_array = [];
 
     // Random greeting
-    $('#frontpage #reload').click(function() {
-        var rand_num = randNum(greeting);
+    $('#reload').click(function() {
+        var rand_num = randNum();
         old_rand_num_array.push(rand_num);
         if(old_rand_num_array.length > (greeting.length*0.7)){
             old_rand_num_array = old_rand_num_array.slice(1);
@@ -60,23 +49,11 @@ $(document).ready(function(){
         $('header').bigtext();
         return false;
     });
-    
-    // Random error
-    $('#error #reload').click(function() {
-        var rand_num = randNum(error);
-        old_rand_num_array.push(rand_num);
-        if(old_rand_num_array.length > (error.length*0.7)){
-            old_rand_num_array = old_rand_num_array.slice(1);
-        }
-        $('#header').text(error[rand_num]);
-        $('header').bigtext();
-        return false;
-    });
 
-    function randNum(array) {
-        var rand_num = Math.floor(Math.random()*(array.length));
-        if($.inArray(rand_num, old_rand_num_array) !== -1 ){ // if number is in array
-            return randNum(array);
+    function randNum() {
+        var rand_num = Math.floor(Math.random()*(greeting.length - 1));
+        if($.inArray(rand_num, old_rand_num_array) !== -1){ // if number is in array
+            return randNum();
         } else { // number is not in array
             return rand_num;
         }
