1d2927995b27205700043d5327db861a287a53d1
Jonathan Kim <jkimbo@gmail.com>
2011-10-29 15:24:47 +0100

    Reverted back to server commit

--- js/script.js
+++ js/script.js
@@ -36,11 +36,22 @@ $(document).ready(function(){
         "Salutations"
     ];
 
+    var error = [
+        "Oh poo!",
+        "Goodness gracious great balls of fire!",
+        "Well this is embarrassing...",
+        "Errmm...",
+        "F***ing hell!",
+        "Oh man!",
+        "Shoot!",
+        "Dagnabbit!"
+    ];
+
     var old_rand_num_array = [];
 
     // Random greeting
-    $('#reload').click(function() {
-        var rand_num = randNum();
+    $('#frontpage #reload').click(function() {
+        var rand_num = randNum(greeting);
         old_rand_num_array.push(rand_num);
         if(old_rand_num_array.length > (greeting.length*0.7)){
             old_rand_num_array = old_rand_num_array.slice(1);
@@ -49,11 +60,23 @@ $(document).ready(function(){
         $('header').bigtext();
         return false;
     });
+    
+    // Random error
+    $('#error #reload').click(function() {
+        var rand_num = randNum(error);
+        old_rand_num_array.push(rand_num);
+        if(old_rand_num_array.length > (error.length*0.7)){
+            old_rand_num_array = old_rand_num_array.slice(1);
+        }
+        $('#header').text(error[rand_num]);
+        $('header').bigtext();
+        return false;
+    });
 
-    function randNum() {
-        var rand_num = Math.floor(Math.random()*(greeting.length - 1));
-        if($.inArray(rand_num, old_rand_num_array) !== -1){ // if number is in array
-            return randNum();
+    function randNum(array) {
+        var rand_num = Math.floor(Math.random()*(array.length));
+        if($.inArray(rand_num, old_rand_num_array) !== -1 ){ // if number is in array
+            return randNum(array);
         } else { // number is not in array
             return rand_num;
         }
